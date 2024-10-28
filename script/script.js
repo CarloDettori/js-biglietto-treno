8c568501dfc16ff257b3578f2a82a32ed3b962cb
CarloDettori <dettoric07@gmail.com>
2024-10-28 16:58:06 +0100

    reset + main variable definition

--- script/script.js
+++ script/script.js
@@ -1,4 +1,10 @@
+"use strict"
+console.clear();
 //- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let km = prompt("inserisci in km la distanza che desideri percorrere");
+let discount = "";
+const Price = km * 0.21 - discount;
+
 //- va applicato uno sconto del 20% per i minorenni
 //- va applicato uno sconto del 40% per gli over 65.
 //- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
